d9307a06853b21263186326f2d2d3123ab4cb5ed
juliatogr <julia.tortosa01@estudiant.upf.edu>
2022-05-22 15:58:25 +0200

    Squashed commit of the following:
    commit aaf6fc3681158c6ee5cd42c6f28e74bb4461bf63
    Merge: d2766ea a29edee
    Author: juliatogr <72571435+juliatogr@users.noreply.github.com>
    Date:   Sun May 22 15:56:02 2022 +0200
    
        Merge pull request #5 from juliatogr/Development
    
        code fixed
    
    commit d2766ea18c44a2a7ad3f425feb9a89b7872ce027
    Merge: e699d60 efd2ce2
    Author: juliatogr <72571435+juliatogr@users.noreply.github.com>
    Date:   Sun May 22 15:41:11 2022 +0200
    
        Merge pull request #4 from juliatogr/Assets
    
        Assets
    
    commit efd2ce2cc78ad46c3b25d7547e91ec71d9157320
    Merge: e6d2f0c e699d60
    Author: juliatogr <julia.tortosa01@estudiant.upf.edu>
    Date:   Sun May 22 15:40:57 2022 +0200
    
        all indiv assets
    
    commit e699d60e12294a2d388a198d980627b8dac1d926
    Merge: 754b1de 938ec95
    Author: juliatogr <72571435+juliatogr@users.noreply.github.com>
    Date:   Sun May 22 15:35:02 2022 +0200
    
        Merge pull request #3 from juliatogr/createAssets/ChemicalLab
    
        ObjetosSeparados
    
    commit 938ec958a61d4e0c44a6b30b828e1e290917315a
    Author: mfranchescagonzalezcejas <102604934+mfranchescagonzalezcejas@users.noreply.github.com>
    Date:   Sat May 21 16:24:49 2022 +0200
    
        ObjetosSeparados
    
    commit e6d2f0c277fa41944342a6f51d1bb30e9114c4ba
    Merge: 754b1de 82976f5
    Author: juliatogr <72571435+juliatogr@users.noreply.github.com>
    Date:   Sat May 21 15:19:36 2022 +0200
    
        Merge pull request #2 from juliatogr/createAssets/structure
    
        lab template
    
    commit 82976f5216acf98f40718f907f25e7367bd761ad
    Author: juliatogr <julia.tortosa01@estudiant.upf.edu>
    Date:   Sat May 21 15:18:54 2022 +0200
    
        lab template

--- src/game.cpp
+++ src/game.cpp
@@ -256,7 +256,7 @@ void RenderPlanes() {
 void Game::render(void)
 {
 	//set the clear color (the background color)
-	glClearColor(1.0, 1.0, 1.0, 1.0);			// white background to simulate the ceiling
+	glClearColor(0.0, 0.0, 0.0, 1.0);			// white background to simulate the ceiling
 
 	// Clear the window and the depth buffer
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
